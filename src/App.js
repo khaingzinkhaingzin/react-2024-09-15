aff2ac0dc40bab8b450438465b49b4190991eb0e
khaingzinkhaingzin <khaingthazin8july@gmail.com>
2024-09-15 14:47:24 +0630

    props.children

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greet name="Bruce">
+        <p>This is children props.</p>
+      </Greet>
+      <Greet name="Clark">
+        <p>This is paragraph for Clark.</p>
+        <button>Action</button>
+      </Greet>
+      <Greet name="Diana" />
     </div>
   );
 }
